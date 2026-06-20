4dcb578eca5da14f5b83632eded7e467f2934991
TANUJ BHAWARIYA <gokultanuj2005@gmail.com>
2026-06-20 16:42:28 +0530

    dsa prac. 1-basic datatypes

--- DSA_in_C++/love_babbar_supereme_batch_dsa/practice/basics/1.cpp
+++ DSA_in_C++/love_babbar_supereme_batch_dsa/practice/basics/1.cpp
@@ -8,11 +8,67 @@
 // namespace - particular region in which scope of identifiers is defined
 //endl or \n - for new line
 //cin - input from user
+
+/* #include<iostream>
+using namespace std;
+int main(){
+    cout << "SNPFIITJEEGAURAV: " << endl;
+    int a;
+    cin >> a;
+    cout << "SNPFIITJEEGAURAV: " << a << endl;
+} */
+
+/* #include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    cout << "value of a is: " << a << endl;
+} */
+
+/* #include<iostream>
+using namespace std;
+int main(){
+    int a=7;
+    cout << sizeof(a) << endl;
+    short b=14;
+    cout << sizeof(b) << endl;
+    float c=21;
+    cout << sizeof(c) << endl;
+    double d=28;
+    cout << sizeof(d) << endl;
+    long e=35;
+    cout << sizeof(e) << endl;
+    bool f=42;
+    cout << sizeof(f) << endl;
+    long long g=49;
+    cout << sizeof(g) << endl;
+    cout << "value of a is: " << a << endl;
+    char ch='g'; 
+    cout << "value of ch is: " << a << endl;
+    bool flag=true;
+    cout << "value of flag is: " << flag << endl;
+} */
+
+/* //implicit type conversion:
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "SNPFIITJEEGAURAV" << endl;
-    int a;
-    cin >> a;
-    cout << "you entered" << a << endl;
+    char ch=2024;
+    cout << ch << endl;
+    int num='g';
+    cout << num << endl;
+} */
+
+//explicit type conversion:
+#include<iostream>
+using namespace std;
+int main(){
+    float a=(float)2;
+    cout << sizeof(a) << endl;
+    long long g=(long long)2;
+    cout << sizeof(g) << endl;
+    double d=5.7;
+    int x=(int)d + 2;
+    cout << ""
 }
+
